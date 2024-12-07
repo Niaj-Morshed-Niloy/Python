9e728fc50c5731c6d3249884b04bd945c6e8deb1
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-07 20:48:26 +0600

    Add files via upload

---
+++ Simple Problem/problem15.py
@@ -0,0 +1,17 @@
+#Write a program to accept marks of 6students and display them ina a sorted manner.
+
+marks=[]
+f1=int(input())
+marks.append(f1)
+f2=int(input())
+marks.append(f2)
+f3=int(input())
+marks.append(f3)
+f4=int(input())
+marks.append(f4)
+f5=int(input())
+marks.append(f5)
+f6=int(input())
+marks.append(f6)
+marks.sort()
+print(marks)
---
+++ Simple Problem/problem16.py
@@ -0,0 +1,4 @@
+#write a python program where count 0 from the following tupple
+
+a=(1,2,3,45,5,6,7,8,9,0,5,67,8,99,0,0,0,0,0,0,0,0,0,0,7,5,55,5)
+print(a.count(0))
