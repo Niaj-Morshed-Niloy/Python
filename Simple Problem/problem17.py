eaf8bd54e8d7752bc3cd9e65520ed7e43493e715
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-08 22:36:40 +0600

    Add files via upload

---
+++ Simple Problem/problem17.py
@@ -0,0 +1,10 @@
+#write a program to create a dictionary of Hindi words with values as their English translarion. Provide user with an option to look it up!
+
+words={
+    "madad":"help",
+    "billi":"cat",
+    "kursi":"chair"
+}
+
+word=input()
+print(words[word])
