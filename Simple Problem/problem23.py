eac6b7e5659bb4ac4494b3aac8dadb14f64baebb
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-10 19:16:45 +0600

    Add files via upload

---
+++ Simple Problem/problem23.py
@@ -0,0 +1,6 @@
+#Write a program to find whether a giver username contain less than 10 char or not
+username=input()
+if(len(username)<10):
+    print("Contain less than 10 char")
+else:
+    print("More than 10")
