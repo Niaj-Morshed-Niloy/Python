d41571bd3f33c5bf7ce38de7991fde7c0131970f
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-10 19:17:06 +0600

    Add files via upload

---
+++ Simple Problem/problem24.py
@@ -0,0 +1,7 @@
+#write a program which finds out whether a given name is present in a list or not.
+a=["Ann","Niaj","Divya"]
+name=input("Enter your name: ")
+if(name in a):
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
