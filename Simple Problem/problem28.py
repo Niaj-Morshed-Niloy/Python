e1557ace4267a618f3fa22c80fea4b5b178ea4e3
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-12 22:58:06 +0600

    Add files via upload

---
+++ Simple Problem/problem28.py
@@ -0,0 +1,11 @@
+# Write a program to greet all the namer stored in a list"l" and which starts with A.
+l=["Anon","Niaj","Ann","Ashikuzaman","Chiknii","potka"]
+
+for name in l:
+    if(name.startswith("A")):
+        print(f"Hey {name}")
+    
+    # else:
+    #     print(f"Muri kha {name}")
+ 
+
