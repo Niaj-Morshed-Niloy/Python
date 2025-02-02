bb17056503074db7f48586b46c6320f691f61b7b
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-02-02 23:07:04 +0600

    Add files via upload

---
+++ Simple Problem/problem64.py
@@ -0,0 +1,13 @@
+#Write a program to display a/b where 
+# a and b are integers. If b=0 display
+#infinte by handling athe 'zeroDivisionError'
+
+
+try:
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a/b)
+
+
+except ZeroDivisionError as v:
+    print("Infinite")
